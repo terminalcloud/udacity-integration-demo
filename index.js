a3259000ced6114729f03dbe5b7a27d445caa9d6
Jesse Swidler <jswidler@gmail.com>
2016-12-07 17:55:05 -0800

    Add getTerminal code

--- index.js
+++ index.js
@@ -73,5 +73,42 @@ function render(serverUrl) {
   ReactDOM.render(<Project serverUrl={parseUrl(serverUrl)}/>, document.getElementById('root'));
 }
 
-render(':8282');
+function getTerminalAddress(accept, reject) {
+
+  function getHardCodedCookie() {
+      return "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE0ODM3Mzc1ODgsImlhdCI6MTQ4MTE0NTU4OCwia2lkIjoiYTdlODllZDIxIiwidWlkIjoiNzQyODUyNjA0NiJ9.LNoDj5rJ-XLj23pqrhv93giWID0Kv90i1vLhqBXyHJOQRa3E2kjzpbZUa1W8oF-VbRbKELTZxO15-QZw3UhEy_ElckH-u3nIHYBLX78E-QZt6BOMH8S7XIWl4C12Hlh_tv-AeObY10eK0OsjoHAeWcdlQZ-m0Uj8ReqZjZGK0uScxHD7uBinIJFejId9b0_f_o6MJTt3NfOSgl5bs6rlmP6gkA1lNMBJ871r7v-V1xQsWnhUtf0rH5zy1rC_ptV_mrhVBZmqkbsIDoVqqGzovIiJ_ryDtDc2ph1D3snd13hXynuxG3gwddvOIP7eZAPCbAQHQpUGluA9zETG";
+  }
+
+  var url = "https://www.jesse2.test-cluster.com/udacity/api/terminal/link"
+  var jwt = getHardCodedCookie("_jwt");
+  var projectId = "f4862cba-db41-4250-bc28-7aaf61f00453";
+
+
+
+  var data = {
+      udacityJwt: jwt,
+      projectId: projectId
+  };
+
+  var xhr = new XMLHttpRequest();
+
+  xhr.addEventListener("readystatechange", function () {
+    if (this.readyState === 4) {
+      accept(JSON.parse(this.responseText)['link']);
+    }
+  });
+
+  xhr.open("POST", url);
+  xhr.setRequestHeader("content-type", "application/json");
+  xhr.setRequestHeader("accept", "application/json");
+
+  xhr.send(JSON.stringify(data));
+}
+
+new Promise(getTerminalAddress).then((res) => {
+  console.log("terminal address: " + res)
+  render(res + ':443');
+});
+
+
 
