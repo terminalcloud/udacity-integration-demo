ea237a67689a5e43628c1ebff159ead92523d842
Nate Tate <nate91711@gmail.com>
2016-12-08 14:25:20 -0800

    adding an id to iframe to be able to remove border

--- bookmarklet/bookmarklet.js
+++ bookmarklet/bookmarklet.js
@@ -1,7 +1,5 @@
-// try this on: https://classroom.udacity.com/nanodegrees/nd200/parts/24c5c39a-6040-4d5a-82d8-7d3d30d7bc9a/project
-
 var cdn_url = 'https://storage.googleapis.com/edu-content/0.0.13/public';
-var terminalClientLink     = cdn_url + '/lib/terminalClient.js';
+var terminalClientLink = cdn_url + '/lib/terminalClient.js';
 
 function injectScriptAndUse(srcUrl, cb) {
   var head = document.getElementsByTagName("head")[0];
@@ -17,9 +15,9 @@ function insertSnippet() {
     jQuery.getScript(terminalClientLink, function() {
       var jwt = terminalClient.getCookie('_jwt_token');
 
-      var test = '<iframe id="terminal" src="http://localhost:3000" frameborder="0" width="100%" height="500px" />';
+      var test = '<iframe id="terminal-ide" src="http://localhost:3000" frameborder="0" width="100%" height="500px" />';
       jQuery('#main-layout-content').first().append(test);
-      jQuery('iframe#terminal').attr("frameborder", 0)
+      jQuery('#terminal-ide').attr("frameborder", 0)
     });
   });
 }
