77975d341d4550dd3ed4ce0717a63b92c4a869e6
Nate Tate <nate91711@gmail.com>
2016-12-12 15:22:30 -0800

    changes the html to encapsulate the three main pieces project, demo text, and terminal only adds left padding to full project repl preserves padding in runcode button and terminal-only removes double borders in full project

--- project.full.js
+++ project.full.js
@@ -42,76 +42,80 @@ module.exports = function ({ bootstrap, PanelManager, Terminal, Editor, Files, L
         width: '100%'
       }
 
-      return <div className='theme_dark' style={layoutBoxStyle}>
-        <Layout layout={{
-          override: true,
-          is_hidden: {},
-          maximized: '',
-          layout: {
-            type: 'vertical',
-            parts: [
-              {
-                type: 'horizontal',
+      return (
+        <div className="multiple-components">
+          <div className='full-project theme_dark' style={layoutBoxStyle}>
+            <Layout layout={{
+              override: true,
+              is_hidden: {},
+              maximized: '',
+              layout: {
+                type: 'vertical',
                 parts: [
                   {
-                    component: <Files manager={this.filesManager}
-                                      editorManager={this.editorManager}
-                                      serverUrl={this.props.serverUrl}/>,
-                    key: 'files',
-                    weight: 2
-                  },
-                  {
-                    component: <Editor manager={this.editorManager}
-                                       filesManager={this.filesManager}
-                                       serverUrl={this.props.serverUrl}/>,
-                    key: 'editor',
-                    weight: 6
-                  },
-               ],
-               weight: 6
-             },
-                  {
-                    component: <Terminal manager={this.terminalManager} serverUrl={this.props.serverUrl}/>,
-                    key: 'terminal',
-                    weight: 6
-                  },
-                  {
-                    component: <div><div id="runcode_container" className="panel"><button className="btn btn-primary" onClick={() => this.run()}>Run Code!</button></div></div>,
-                    key: 'run-button',
-                    weight: 1
-                  }
-            ]
-          }
-        }}/>
-
-      <br/>
-      <p style={{ position: 'relative', top: 680 }}>
-        Let's focus on how python  math actually works. When you're working in Python you can use 
-        the "math" library to access many functions to speed your everyday computations along. For
-        instance, you can use ceil(), floor(), and round() to convert fractional numbers to nice round
-        integers in a predictable way.
-
-        To show this off, let's play with a few of these in the python interpreter below. Enter math.ceil(4.5) into
-         the interpreter and see what it says. Then try math.floor(4.5). Did this do what you'd expect?
-      </p>
-
-      <div style={{ top: 800, position: 'relative', height: 300, marginBottom: 200 }}>
-        <Layout layout={{
-            is_hidden: {},
-            maximized: '',
-            layout: {
-              type: 'horizontal',
-              parts: [
-                {
-                  component: <Terminal manager={this.terminal2Manager} serverUrl={this.props.serverUrl}/>,
-                  key: 'terminal',
-                  weight: 1
+                    type: 'horizontal',
+                    parts: [
+                      {
+                        component: <Files manager={this.filesManager}
+                                          editorManager={this.editorManager}
+                                          serverUrl={this.props.serverUrl}/>,
+                        key: 'files',
+                        weight: 2
+                      },
+                      {
+                        component: <Editor manager={this.editorManager}
+                                           filesManager={this.filesManager}
+                                           serverUrl={this.props.serverUrl}/>,
+                        key: 'editor',
+                        weight: 6
+                      },
+                   ],
+                   weight: 6
+                 },
+                      {
+                        component: <Terminal manager={this.terminalManager} serverUrl={this.props.serverUrl}/>,
+                        key: 'terminal',
+                        weight: 6
+                      },
+                      {
+                        component: <div><div id="runcode_container" className="panel"><button className="btn btn-primary" onClick={() => this.run()}>Run Code!</button></div></div>,
+                        key: 'run-button',
+                        weight: 1
+                      }
+                ]
+              }
+            }}/>
+          </div>
+          <article className="demo-text" style={{ position: 'relative', top: 680 }}>
+            <p>
+              Let's focus on how python  math actually works. When you're working in Python you can use
+              the "math" library to access many functions to speed your everyday computations along. For
+              instance, you can use ceil(), floor(), and round() to convert fractional numbers to nice round
+              integers in a predictable way.
+            </p>
+            <p>
+              To show this off, let's play with a few of these in the python interpreter below. Enter math.ceil(4.5) into
+               the interpreter and see what it says. Then try math.floor(4.5). Did this do what you'd expect?
+            </p>
+          </article>
+          <div className="terminal-only" style={{ top: 800, position: 'relative', height: 300, marginBottom: 200 }}>
+            <Layout layout={{
+                is_hidden: {},
+                maximized: '',
+                layout: {
+                  type: 'horizontal',
+                  parts: [
+                    {
+                      component: <Terminal manager={this.terminal2Manager} serverUrl={this.props.serverUrl}/>,
+                      key: 'terminal',
+                      weight: 1
+                    }
+                  ]
                 }
-              ]
-            }
-          }}/>
-      </div>
-      </div>
+              }}/>
+          </div>
+        </div>
+      )
     }
   }
 
