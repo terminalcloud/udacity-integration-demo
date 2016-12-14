761dcbb36751c7df68d1085d746b50b647afb0e8
Jesse Swidler <jswidler@gmail.com>
2016-12-14 14:56:57 -0800

    Fix shell window issues

--- project.full.js
+++ project.full.js
@@ -28,17 +28,19 @@ module.exports = function ({ bootstrap, PanelManager, Terminal, Editor, Files, L
         type: 'POST',
         url: this.props.serverUrl + '/exec',
         data: JSON.stringify({ cmd: fixtureCommand}),
-        success: () => { this.addTestFile(); this.run() }
-      })
+        success: () => { 
+          this.addTestFile(); 
+          this.run('persistence.py'); }
+      });
+
+      this.terminal2Manager.destroyTerm('repl');
+      this.terminal2Manager.newTerm('repl', '/usr/bin/python', [], () => this.terminal2Manager.SelectTab('repl'));
     }
 
     run(file) {
       this.editorManager.saveAllFiles().then(() => {
-        this.terminalManager.destroyTerm(this.terminalManager.getCurrentTermId())
-        this.terminalManager.newTerm('test code', '/usr/bin/python', ['-i', '/home/' + file])
-
-        this.terminal2Manager.destroyTerm('repl')
-        this.terminal2Manager.newTerm('repl', '/usr/bin/python', [], () => this.terminal2Manager.SelectTab('repl'))
+        this.terminalManager.destroyTerm('test code')
+        this.terminalManager.newTerm('test code', '/usr/bin/python', ['-i', '/home/' + file],  () => this.terminalManager.SelectTab('test code'))
       })
     }
 
